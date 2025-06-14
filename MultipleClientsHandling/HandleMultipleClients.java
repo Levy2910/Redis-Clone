8eab683a3094ea80661fbb5e9996522c33207387
Le Vy Cao <levycaoesc14@gmail.com>
2025-06-14 14:52:56 +0700

    connect for multiple clients using runnable

---
+++ MultipleClientsHandling/HandleMultipleClients.java
@@ -0,0 +1,17 @@
+import MultipleClientsHandling.LetMeHandleThisClient;
+
+public static void main(String[] args) throws IOException {
+    ServerSocket serverSocket = new ServerSocket(3003);
+    System.out.println("Server started on port 3003");
+
+    while (true) {
+        try {
+            Socket clientSocket = serverSocket.accept();
+            LetMeHandleThisClient letMeHandleThisClient = new LetMeHandleThisClient(clientSocket);
+            Thread newThread = new Thread(letMeHandleThisClient);
+            newThread.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
