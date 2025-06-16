f788d6c0277ccef196eb6c4e4918caee27025da1
Le Vy Cao <levycaoesc14@gmail.com>
2025-06-17 00:52:46 +0700

    refactor code for better readability

--- day6_SimpleRedisClone/HandleRequest.java
+++ day6_SimpleRedisClone/HandleRequest.java
@@ -25,52 +25,61 @@ public class HandleRequest implements Runnable{
                 String line = bufferedReader.readLine();
                 if (line == null || (line.equals("bye"))) {
                     bufferedWriter.write("ok bye, see ya :3 "  + "\n");
+                    bufferedWriter.flush();
                     break;
                 }
                 // extract line
                 String[] extractedLine = line.toLowerCase().split(" ");
-                if (!(extractedLine[0].equals("get") || extractedLine[0].equals("set") || extractedLine[0].equals("del"))){
-                    bufferedWriter.write("invalid command "  + "\n");
-                    bufferedWriter.flush();
-                    continue;
-                }
-                if (extractedLine[0].equals("get") && extractedLine.length != 2){
-                    bufferedWriter.write("invalid command "  + "\n");
-                    bufferedWriter.flush();
-                    continue;
-                }
-                if (extractedLine[0].equals("del") && extractedLine.length != 2){
-                    bufferedWriter.write("invalid command "  + "\n");
-                    bufferedWriter.flush();
-                    continue;
-                }
-                if (extractedLine[0].equals("set") && extractedLine.length != 3){
-                    bufferedWriter.write("invalid command " + "\n");
-                    bufferedWriter.flush();
-                    continue;
-                }
 
-                if(extractedLine[0].equals("get")){
-                    if (storeRedis.get(extractedLine[1]) != null){
-                        bufferedWriter.write("the value from key " + extractedLine[1] + "is :" + storeRedis.get(extractedLine[1]) + "\n");
-                    }else{
-                        bufferedWriter.write("key not exist "  + "\n");
-                    }
+                String command = extractedLine[0];
+                String key;
+                switch (command){
+                    case "get":
+                        if (extractedLine.length != 2){
+                            bufferedWriter.write("invalid command " + "\n");
+                            bufferedWriter.flush();
+                            continue;
+                        }
+                        key = extractedLine[1];
+                        if (storeRedis.get(key) == null){
+                            bufferedWriter.write("there is no such key" + "\n");
+                            bufferedWriter.flush();
+                            continue;
+                        }
+                        bufferedWriter.write("value: " + storeRedis.get(key) + "\n");
+                        bufferedWriter.flush();
+                        continue;
+                    case "del":
+                        if (extractedLine.length != 2){
+                            bufferedWriter.write("invalid command " + "\n");
+                            bufferedWriter.flush();
+                            continue;
+                        }
+                        key = extractedLine[1];
+                        if (storeRedis.get(key) == null){
+                            bufferedWriter.write("there is no such key " + "\n");
+                            bufferedWriter.flush();
+                            continue;
+                        }
+                        storeRedis.del(key);
+                        bufferedWriter.write("already deleted key " +"\n");
+                        bufferedWriter.flush();
+                        continue;
+                    case "set":
+                        if (extractedLine.length != 3){
+                            bufferedWriter.write("invalid command " + "\n");
+                            bufferedWriter.flush();
+                            continue;
+                        }
+                        key = extractedLine[1];
+                        storeRedis.set(key, extractedLine[2]);
+                        bufferedWriter.write("already added key "+ "\n");
+                        bufferedWriter.flush();
+                        continue;
+                    default:
+                        bufferedWriter.write("invalid command "+ "\n");
+                        bufferedWriter.flush();
                 }
-                if(extractedLine[0].equals("del")){
-                    if (storeRedis.get(extractedLine[1]) != null){
-                        storeRedis.del(extractedLine[1]);
-                        bufferedWriter.write("deleted key " + extractedLine[1] + "\n");
-                    }else{
-                        bufferedWriter.write("key not exist "  + "\n");
-                    }
-                }
-                if (extractedLine[0].equals("set")){
-                    storeRedis.set(extractedLine[1], extractedLine[2]);
-                    bufferedWriter.write("added key "+ storeRedis.get(extractedLine[1])  + "\n");
-                }
-
-                bufferedWriter.flush();
             }
 
             clientSocket.close();
